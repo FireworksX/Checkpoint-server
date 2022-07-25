747885723dbb38bee435d649e3f1612f4a9fe66c
arturabeltins <abeltinsh.a@icloud.com>
2022-07-25 20:43:11 +0300

    change server

--- src/index.ts
+++ src/index.ts
@@ -7,32 +7,33 @@ moduleAlias.addAliases({
 });
 
 import { createServer } from '@config/express';
-import { AddressInfo } from 'net';
-import http from 'http';
 import { logger } from '@config/logger';
 import { createMongooseConnection } from '@config/mongoose';
 
-const host = process.env.HOST || '0.0.0.0';
 const port = process.env.PORT || '5000';
 
 async function startServer() {
   await createMongooseConnection();
 
   const app = createServer();
-  console.log(host, port);
-  const server = http.createServer(app).listen({ host, port }, () => {
-    const addressInfo = server.address() as AddressInfo;
-    logger.info(`Server ready at http://${addressInfo.address}:${addressInfo.port}`);
-  });
+
+  app.listen(port, () => {
+    logger.info(`Server ready at http://localhost:${port}`);
+  })
+
+  // const server = http.createServer(app).listen({ host, port }, () => {
+  //   const addressInfo = server.address() as AddressInfo;
+  //   logger.info(`Server ready at http://${addressInfo.address}:${addressInfo.port}`);
+  // });
 
   const signalTraps: NodeJS.Signals[] = ['SIGTERM', 'SIGINT', 'SIGUSR2'];
   signalTraps.forEach((type) => {
     process.once(type, async () => {
       logger.info(`process.once ${type}`);
 
-      server.close(() => {
-        logger.debug('HTTP server closed');
-      });
+      // server.close(() => {
+      //   logger.debug('HTTP server closed');
+      // });
     });
   });
 }
